c70b79f0eb7bdadb6e983df973ce2518786d39ce
root <root@epbyminw6406.minsk.epam.com>
2017-04-11 15:00:13 +0300

    Final

--- cookbooks/httpd/attributes/default.rb
+++ cookbooks/httpd/attributes/default.rb
@@ -1,4 +1,4 @@
 default['httpd']['vhost'] = "/etc/httpd/conf.d/vhosts.conf"
 default['httpd']['trouble'] = "/etc/httpd/conf.d/trouble.conf"
-default['httpd']['doc'] = "/home/apache/htdocs/"
+default['httpd']['doc'] = "/var/www/html/"
 default['httpd']['maintenance_file'] = "#{node['httpd']['doc']}/maintenance.html"
